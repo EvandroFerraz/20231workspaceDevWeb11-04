077013db6fa499ab3f7161abc21950265f1d793a
evandro <ferraz.evandro@outlook.com>
2023-04-12 00:41:47 -0300

    11-04

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import React from "react"
+import ReactDOM from "react-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            latitude: null,
+            longitude: null,
+            estacao: null,
+            data: null,
+            icone: null
+        }
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    obterEstacao = (data,latitude) => {
+        const anoAtual = data.getFullYear()
+        //new Date(ano, mes (0 até 11), dia (1 até 31))
+        //21/06
+        const d1 = new Date(anoAtual,5,21)
+        //24/09
+        const d2 = new Date(anoAtual,8,24)
+        //22/12
+        const d3 = new Date(anoAtual,11,22)
+        //21/03
+        const d4 = new Date(anoAtual, 2, 21)
+
+        if(data >= d1 && data < d2)
+            // se latitude < 0 entao esta no hemisferio SUL
+            return latitude < 0 ? "Inverno" : "Verão"
+        if(data >= d2 && data < d3)
+            return latitude < 0 ? "Primavera" : "Outono"
+        if(data >= d3 || data < d4)
+            return latitude < 0 ? "Verão" : "Inverno"
+        return latitude < 0 ? "Outono" : "Primavera"    
+    }
+
+    icones = {
+        "Primavera" : "fa-seedling",
+        "Verão" : "fa-umbrella-beach",
+        "Outono" : "fa-tree",
+        "Inverno" : "fa-snowman"
+    }
+
+    obterLocalizacao = () =>{
+        window.navigator.geolocation.getCurrentPosition(
+            (posicao) => {
+                let data = new Date()
+                let estacao = this.obterEstacao(data,posicao.coords.latitude)
+                let icone = this.icones[estacao]
+                this.setState(
+                    {
+                        latitude: posicao.coords.latitude,
+                        longitude: posicao.coords.longitude,
+                        estacao: estacao,
+                        data: data.toLocaleTimeString(),
+                        icone: icone
+                    }
+                )
+            }
+        )
+    }
+
+    render(){
+        return(<div>Meu app</div>)
+    } 
+}
+
+ReactDOM.render(<App/>,document.querySelector("#root"))
